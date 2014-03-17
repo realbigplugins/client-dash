cc279bd20cb3e4c7067119d1a6da8ddd789d3930
Kyle <kyle@realbigmarketing.com>
2014-03-17 15:47:23 -0400

    Outlining the tasks
    Just new comments.

--- client-dash.php
+++ client-dash.php
@@ -7,4 +7,16 @@ Author: Kyle Maurer
 Author URI: http://realbigmarketing.com/staff/kyle
 */
 
+// Remove all dashboard widgets
+
+
+// Add new dashboard widgets
+
+
+// Create admin pages
+
+
+// Create settings page
+
+
 ?>
