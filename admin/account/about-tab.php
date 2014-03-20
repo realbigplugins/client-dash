4b6c196e91935765dccacbf4834a8df5e1a07a8b
Kyle <kyle@realbigmarketing.com>
2014-03-20 19:35:23 -0400

    Added some user info on about tab
    Just output some profile info about current_user

--- admin/account/about-tab.php
+++ admin/account/about-tab.php
@@ -1,3 +1,32 @@
 <?php
-echo "this is the about tab";
+global $current_user;
+get_currentuserinfo();
+$cd_username = $current_user->user_login;
+$cd_firstname = $current_user->first_name;
+$cd_lastname = $current_user->last_name;
+$cd_useremail = $current_user->user_email;
+$cd_userregistered = $current_user->user_registered;
+$cd_userrole = 'STILL NEED THIS FUNCTION';
 ?>
+<table class="form-table">
+	<tr valign="top">
+		<th scope="row">Your username</th>
+		<td><?php echo $cd_username; ?></td>
+	</tr>
+	<tr valign="top">
+		<th scope="row">Your name</th>
+		<td><?php echo $cd_firstname.' '.$cd_lastname; ?></td>
+	</tr>
+	<tr valign="top">
+		<th scope="row">Your e-mail address</th>
+		<td><?php echo $cd_useremail; ?></td>
+	</tr>
+	<tr valign="top">
+		<th scope="row">When you first joined this site</th>
+		<td><?php echo $cd_userregistered; ?></td>
+	</tr>
+	<tr valign="top">
+		<th scope="row">Your role</th>
+		<td><?php echo $cd_userrole; ?></td>
+	</tr>
+</table>
