23ecf5a35feb41c56807e29867726914209c885d
Kyle <kyle@realbigmarketing.com>
2014-05-04 10:50:17 -0400

    Remove Woocommerce widgets
    If WC is active, remove three widgets. Not positive I got the recent
    orders one correct.

--- widgets/ditch-widgets.php
+++ widgets/ditch-widgets.php
@@ -6,6 +6,11 @@ function cd_ditch_default_widgets() {
 	remove_meta_box('dashboard_activity', 'dashboard', 'normal');
 	remove_meta_box('dashboard_quick_press', 'dashboard', 'side');
 	remove_meta_box('dashboard_primary', 'dashboard', 'side');
+	if (class_exists('Woocommerce')){
+		remove_meta_box('woocommerce_dashboard_status', 'dashboard', 'normal');
+		remove_meta_box('woocommerce_dashboard_recent_orders', 'dashboard', 'normal');
+		remove_meta_box('woocommerce_dashboard_recent_reviews', 'dashboard', 'normal');
+	}
 }
 }
 add_action('wp_dashboard_setup', 'cd_ditch_default_widgets');
