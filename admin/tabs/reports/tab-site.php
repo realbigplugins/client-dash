783d05a962105d95a379716273d0f863874b1955
Kyle <kyle@realbigmarketing.com>
2014-06-28 13:02:54 -0400

    Link items on site tab, reports page
    Make all 5 link to their respective list pages.

--- admin/tabs/reports/tab-site.php
+++ admin/tabs/reports/tab-site.php
@@ -13,7 +13,11 @@ function cd_core_reports_site_tab() {
 
 	<table class="form-table">
 		<tr valign="top">
-			<th scope="row">Posts</th>
+			<th scope="row">
+				<a href="<?php echo get_admin_url(); ?>edit.php?post_type=post">
+				Posts
+				</a>
+			</th>
 			<td><?php echo $cd_count_posts->publish; ?> published<br/>
 				<?php echo $cd_count_posts->draft; ?> draft<?php if ( $cd_count_posts->draft != 1 ) {
 					echo 's';
@@ -21,13 +25,21 @@ function cd_core_reports_site_tab() {
 			</td>
 		</tr>
 		<tr valign="top">
-			<th scope="row">Comments</th>
+			<th scope="row">
+				<a href="<?php echo get_admin_url(); ?>edit-comments.php">
+				Comments
+				</a>
+			</th>
 			<td><?php echo $cd_count_comments->approved; ?> approved<br/>
 				<?php echo $cd_count_comments->spam; ?> spam
 			</td>
 		</tr>
 		<tr valign="top">
-			<th scope="row">Pages</th>
+			<th scope="row">
+				<a href="<?php echo get_admin_url(); ?>edit.php?post_type=page">
+				Pages
+				</a>
+			</th>
 			<td><?php echo $cd_count_pages->publish; ?> published<br/>
 				<?php echo $cd_count_pages->draft; ?> draft<?php if ( $cd_count_pages->draft != 1 ) {
 					echo 's';
@@ -35,7 +47,11 @@ function cd_core_reports_site_tab() {
 			</td>
 		</tr>
 		<tr valign="top">
-			<th scope="row">Users</th>
+			<th scope="row">
+				<a href="<?php echo get_admin_url(); ?>users.php">
+				Users
+				</a>
+			</th>
 			<td><?php echo $cd_count_users['total_users']; ?> total registered users<br/>
 				<?php foreach ( $cd_count_users['avail_roles'] as $role => $count ) {
 					echo $count . ' ' . $role . '<br/>';
@@ -43,7 +59,11 @@ function cd_core_reports_site_tab() {
 			</td>
 		</tr>
 		<tr valign="top">
-			<th scope="row">Media</th>
+			<th scope="row">
+				<a href="<?php echo get_admin_url(); ?>upload.php">
+				Media
+				</a>
+			</th>
 			<?php
 			$upload_dir  = wp_upload_dir();
 			$dir_info    = cd_get_dir_size( $upload_dir['basedir'] );
