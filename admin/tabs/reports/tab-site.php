a8f051ffd2161c164bc0f2940a10b6b910d81b2a
Kyle <kyle@realbigmarketing.com>
2014-06-30 13:41:13 -0400

    Beefing up Reports
    The post types are now dynamic and include CPTs. They also link to
    their respective lists in the admin.

--- admin/tabs/reports/tab-site.php
+++ admin/tabs/reports/tab-site.php
@@ -5,25 +5,42 @@
  */
 function cd_core_reports_site_tab() {
 	// Get the site information
+	$args = array(
+	'public' => true
+	//'_builtin' => false
+	);
 	$cd_count_posts    = wp_count_posts();
 	$cd_count_pages    = wp_count_posts( 'page' );
 	$cd_count_comments = wp_count_comments();
 	$cd_count_users    = count_users();
+	$cd_post_types	   = get_post_types( $args, 'objects' )
 	?>
 
 	<table class="form-table">
+		<?php foreach ($cd_post_types as $type) {
+			$nice_name = $type->labels->name;
+			$name = $type->name;
+			$count = wp_count_posts( $name );
+			$link = get_admin_url(). 'edit.php?post_type=' .$name;
+
+		if ($name != 'attachment') {
+		?>
 		<tr valign="top">
 			<th scope="row">
-				<a href="<?php echo get_admin_url(); ?>edit.php?post_type=post">
-				Posts
+				<a href="<?php echo $link; ?>">
+				<?php echo $nice_name; ?>
 				</a>
 			</th>
-			<td><?php echo $cd_count_posts->publish; ?> published<br/>
-				<?php echo $cd_count_posts->draft; ?> draft<?php if ( $cd_count_posts->draft != 1 ) {
-					echo 's';
-				} ?>
+			<td>
+				<ul>
+					<li><a href="<?php echo $link. '&post_status=publish'; ?>"><?php echo $count->publish; ?> published</a></li>
+					<li><a href="<?php echo $link. '&post_status=pending'; ?>"><?php echo $count->pending; ?> pending</a></li>
+					<li><a href="<?php echo $link. '&post_status=draft'; ?>"><?php echo $count->draft; ?> drafts</a></li>
+				</ul>
 			</td>
 		</tr>
+		<?php }
+		} ?>
 		<tr valign="top">
 			<th scope="row">
 				<a href="<?php echo get_admin_url(); ?>edit-comments.php">
@@ -32,30 +49,6 @@ function cd_core_reports_site_tab() {
 			</th>
 			<td><?php echo $cd_count_comments->approved; ?> approved<br/>
 				<?php echo $cd_count_comments->spam; ?> spam
-			</td>
-		</tr>
-		<tr valign="top">
-			<th scope="row">
-				<a href="<?php echo get_admin_url(); ?>edit.php?post_type=page">
-				Pages
-				</a>
-			</th>
-			<td><?php echo $cd_count_pages->publish; ?> published<br/>
-				<?php echo $cd_count_pages->draft; ?> draft<?php if ( $cd_count_pages->draft != 1 ) {
-					echo 's';
-				} ?>
-			</td>
-		</tr>
-		<tr valign="top">
-			<th scope="row">
-				<a href="<?php echo get_admin_url(); ?>users.php">
-				Users
-				</a>
-			</th>
-			<td><?php echo $cd_count_users['total_users']; ?> total registered users<br/>
-				<?php foreach ( $cd_count_users['avail_roles'] as $role => $count ) {
-					echo $count . ' ' . $role . '<br/>';
-				} ?>
 			</td>
 		</tr>
 		<tr valign="top">
@@ -74,8 +67,27 @@ function cd_core_reports_site_tab() {
 				(<?php echo $dir_info['count']; ?> files)
 			</td>
 		</tr>
+		<tr valign="top">
+			<th scope="row">
+				<a href="<?php echo get_admin_url(); ?>users.php">
+				Users
+				</a>
+			</th>
+			<td><?php echo $cd_count_users['total_users']; ?> total registered users<br/>
+				<?php foreach ( $cd_count_users['avail_roles'] as $role => $count ) {
+					echo $count . ' ' . $role . '<br/>';
+				} ?>
+			</td>
+		</tr>
 	</table>
 <?php
+$args = array(
+	'public' => true
+	//'_builtin' => false
+	);
+echo '<pre>';
+print_r(get_post_types( $args, 'objects' ));
+echo '</pre>';
 }
 
 cd_content_block( 'Core Reports Site', 'reports', 'site', 'cd_core_reports_site_tab' );
