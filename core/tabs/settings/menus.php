c6629b59fd0d9f2e1f8e17b17d442b19489a0ecf
joelworsham <joelworsham@gmail.com>
2014-09-23 08:40:11 -0400

    Fatal error bug.
    Changed static property to non-static. So had to create a pseudo getter function to retrieve the non-static properties from within the child static methods.

--- core/tabs/settings/menus.php
+++ core/tabs/settings/menus.php
@@ -386,6 +386,23 @@ class ClientDash_Core_Page_Settings_Tab_Menus extends ClientDash {
 	}
 
 	/**
+	 * Gets properties of the class.
+	 *
+	 * @since Client Dash 1.6
+	 *
+	 * @param string $property The property to retrieve.
+	 *
+	 * @return mixed The property.
+	 */
+	public static function _get( $property ) {
+		if ( property_exists( __CLASS__, $property ) ) {
+
+			$vars = get_class_vars( __CLASS__ );
+			return $vars[ $property ];
+		}
+	}
+
+	/**
 	 * Allows filtering of the object's properties.
 	 *
 	 * @since Client Dash 1.6
